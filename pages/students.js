b7c22f74fb947baaaf7a40f40493f455acc00675
Slackin' Jack <41076523+SlackinJack@users.noreply.github.com>
2024-03-12 16:56:04 -0600

    form check test

--- pages/students.js
+++ pages/students.js
@@ -1,9 +1,11 @@
 import Head from "next/head";
 import Image from "next/image";
 import Layout from "@/components/layout";
+import { useForm } from "react-hook-form";
 
 
 export default function Students() {
+    const { register } = useForm();
     return (
         <Layout>
             <Head>
@@ -163,14 +165,39 @@ export default function Students() {
                         <h1 class="text-2xl font-bold title-font mb-4 text-gray-900 tracking-widest">Add New Student</h1>
                     </div>
                     <div class="max-w-lg mx-auto">
-                        <form id="new-student-form" class="mb-6">
+                        <form
+                            id="new-student-form"
+                            class="mb-6"
+                        >
                             <div class="mb-4">
                                 <label for="student-name" class="block text-gray-700 text-sm font-bold mb-2">Full Name:</label>
-                                <input type="text" id="student-name" name="student-name" class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"></input>
+                                <input
+                                    type="text"
+                                    id="student-name"
+                                    name="student-name"
+                                    class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+                                    {...register("student-name", {
+                                        required: {
+                                            value: true,
+                                            pattern: /^[a-zA-Z]{1,}\ [a-zA-Z]{1,}$/,
+                                        },
+                                    })}
+                                />
                             </div>
                             <div class="mb-4">
                                 <label for="date-of-birth" class="block text-gray-700 text-sm font-bold mb-2">Date of Birth:</label>
-                                <input type="date" id="date-of-birth" name="date-of-birth" class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"></input>
+                                <input
+                                    type="date"
+                                    id="date-of-birth"
+                                    name="date-of-birth"
+                                    class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+                                    {...register("date-of-birth", {
+                                        required: {
+                                            value: true,
+                                            message: "This field is required.",
+                                        },
+                                    })}
+                                />
                             </div>
                             <div class="mb-6">
                                 <label for="current-grade" class="block text-gray-700 text-sm font-bold mb-2">Current Grade:</label>
