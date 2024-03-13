68fe346a6b0fcdb81ddcc8a2d5ba732bc0dc9157
Slackin' Jack <41076523+SlackinJack@users.noreply.github.com>
2024-03-12 23:54:49 -0600

    try to fix github-pages again

--- components/footer.js
+++ components/footer.js
@@ -4,12 +4,12 @@ export default function Footer() {
             <footer class="text-gray-600 body-font">
                 <div class="container px-5 py-24 mx-auto flex md:items-center lg:items-start md:flex-row md:flex-nowrap flex-wrap flex-col">
                     <div class="w-64 flex-shrink-0 md:mx-0 mx-auto text-center md:text-left md:mt-0 mt-10">
-                    <Link class="flex title-font font-medium items-center md:justify-start justify-center text-gray-900">
+                    <a class="flex title-font font-medium items-center md:justify-start justify-center text-gray-900">
                         <svg xmlns="http://www.w3.org/2000/svg" fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" class="w-10 h-10 text-white p-2 bg-indigo-500 rounded-full" viewBox="0 0 24 24">
                         <path d="M12 2L2 7l10 5 10-5-10-5zM2 17l10 5 10-5M2 12l10 5 10-5"></path>
                         </svg>
                         <span class="ml-3 text-xl">New Generation High School</span>
-                    </Link>
+                    </a>
                     <p class="mt-2 text-sm text-gray-500">Cultivate future talent.</p>
                     </div>
                     <div class="flex-grow flex flex-wrap md:pr-20 -mb-10 md:text-left text-center order-first">
@@ -17,10 +17,10 @@ export default function Footer() {
                         <h2 class="title-font font-bold text-gray-900 tracking-widest text-sm mb-3">Contact us</h2>
                         <nav class="list-none mb-10">
                         <li>
-                            <Link class="text-gray-600 hover:text-gray-800">403-000-0000</Link>
+                            <a class="text-gray-600 hover:text-gray-800">403-000-0000</a>
                         </li>
                         <li>
-                            <Link class="text-gray-600 hover:text-gray-800">newgeneration@h.s.com</Link>
+                            <a class="text-gray-600 hover:text-gray-800">newgeneration@h.s.com</a>
                         </li>
                         </nav>
                     </div>
@@ -28,16 +28,16 @@ export default function Footer() {
                         <h2 class="title-font font-bold text-gray-900 tracking-widest text-sm mb-3">School Address</h2>
                         <nav class="list-none mb-10">
                         <li>
-                            <Link class="text-gray-600 hover:text-gray-800">123 Main Street,</Link>
+                            <a class="text-gray-600 hover:text-gray-800">123 Main Street,</a>
                         </li>
                         <li>
-                            <Link class="text-gray-600 hover:text-gray-800">Springfield, Anytown,</Link>
+                            <a class="text-gray-600 hover:text-gray-800">Springfield, Anytown,</a>
                         </li>
                         <li>
-                            <Link class="text-gray-600 hover:text-gray-800">State ABC,</Link>
+                            <a class="text-gray-600 hover:text-gray-800">State ABC,</a>
                         </li>
                         <li>
-                            <Link class="text-gray-600 hover:text-gray-800">Canada</Link>
+                            <a class="text-gray-600 hover:text-gray-800">Canada</a>
                         </li>
                         </nav>
                     </div>
@@ -45,22 +45,22 @@ export default function Footer() {
                         <h2 class="title-font font-bold text-gray-900 tracking-widest text-sm mb-3">Hours</h2>
                         <nav class="list-none mb-10">
                         <li>
-                            <Link class="text-gray-600 hover:text-gray-800">Class Hours</Link>
+                            <a class="text-gray-600 hover:text-gray-800">Class Hours</a>
                         </li>
                         <li>
-                            <Link class="text-gray-600 hover:text-gray-800">Mon-Thurs: 8:30-3:30</Link>
+                            <a class="text-gray-600 hover:text-gray-800">Mon-Thurs: 8:30-3:30</a>
                         </li>
                         <li>
-                            <Link class="text-gray-600 hover:text-gray-800">Fri: 8:30-1:30</Link>
+                            <a class="text-gray-600 hover:text-gray-800">Fri: 8:30-1:30</a>
                         </li>
                         <li>
-                            <Link class="text-gray-600 hover:text-gray-800">Office Hours</Link>
+                            <a class="text-gray-600 hover:text-gray-800">Office Hours</a>
                         </li>
                         <li>
-                            <Link class="text-gray-600 hover:text-gray-800">Mon-Thurs: 8:00-3:50</Link>
+                            <a class="text-gray-600 hover:text-gray-800">Mon-Thurs: 8:00-3:50</a>
                         </li>
                         <li>
-                            <Link class="text-gray-600 hover:text-gray-800">Fri: 8:00-1:50</Link>
+                            <a class="text-gray-600 hover:text-gray-800">Fri: 8:00-1:50</a>
                         </li>
                         </nav>
                     </div>
@@ -68,10 +68,10 @@ export default function Footer() {
                         <h2 class="title-font font-bold text-gray-900 tracking-widest text-sm mb-3">Leadership Team</h2>
                         <nav class="list-none mb-10">
                         <li>
-                            <Link class="text-gray-600 hover:text-gray-800">Principal</Link>
+                            <a class="text-gray-600 hover:text-gray-800">Principal</a>
                         </li>
                         <li>
-                            <Link class="text-gray-600 hover:text-gray-800">Alex Johnson</Link>
+                            <a class="text-gray-600 hover:text-gray-800">Alex Johnson</a>
                         </li>
                         </nav>
                     </div>
@@ -80,28 +80,28 @@ export default function Footer() {
                 <div class="bg-gray-100">
                     <div class="container mx-auto py-4 px-5 flex flex-wrap flex-col sm:flex-row">
                     <span class="inline-flex sm:ml-auto sm:mt-0 mt-2 justify-center sm:justify-start">
-                        <Link class="text-gray-500">
+                        <a class="text-gray-500">
                         <svg fill="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" class="w-5 h-5" viewBox="0 0 24 24">
                             <path d="M18 2h-3a5 5 0 00-5 5v3H7v4h3v8h4v-8h3l1-4h-4V7a1 1 0 011-1h3z"></path>
                         </svg>
-                        </Link>
-                        <Link class="ml-3 text-gray-500">
+                        </a>
+                        <a class="ml-3 text-gray-500">
                         <svg fill="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" class="w-5 h-5" viewBox="0 0 24 24">
                             <path d="M23 3a10.9 10.9 0 01-3.14 1.53 4.48 4.48 0 00-7.86 3v1A10.66 10.66 0 013 4s-4 9 5 13a11.64 11.64 0 01-7 2c9 5 20 0 20-11.5a4.5 4.5 0 00-.08-.83A7.72 7.72 0 0023 3z"></path>
                         </svg>
-                        </Link>
-                        <Link class="ml-3 text-gray-500">
+                        </a>
+                        <a class="ml-3 text-gray-500">
                         <svg fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" class="w-5 h-5" viewBox="0 0 24 24">
                             <rect width="20" height="20" x="2" y="2" rx="5" ry="5"></rect>
                             <path d="M16 11.37A4 4 0 1112.63 8 4 4 0 0116 11.37zm1.5-4.87h.01"></path>
                         </svg>
-                        </Link>
-                        <Link class="ml-3 text-gray-500">
+                        </a>
+                        <a class="ml-3 text-gray-500">
                         <svg fill="currentColor" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="0" class="w-5 h-5" viewBox="0 0 24 24">
                             <path stroke="none" d="M16 8a6 6 0 016 6v7h-4v-7a2 2 0 00-2-2 2 2 0 00-2 2v7h-4v-7a6 6 0 016-6zM2 9h4v12H2z"></path>
                             <circle cx="4" cy="4" r="2" stroke="none"></circle>
                         </svg>
-                        </Link>
+                        </a>
                     </span>
                     </div>
                 </div>
